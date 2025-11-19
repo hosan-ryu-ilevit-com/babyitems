581dc1901b28b8abfb6e701af0b16e5c9317aa08
류호산 <hosan.ryu@ilevit.com>
2025-11-19 15:46:58 +0900

    예시상품태그 추가 전

--- app/page.tsx
+++ app/page.tsx
@@ -218,7 +218,7 @@ export default function Home() {
                   <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M13 2L3 14h8l-1 8 10-12h-8l1-8z"/>
                   </svg>
-                  분유포트 1분 가이드
+                  분유포트 시작 가이드
                 </button>
               </div>
             </motion.div>
